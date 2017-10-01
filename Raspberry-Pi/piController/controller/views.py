ebef10817a3f5018eb97a240dc85f8ed6ceb004e
Gilberto Martinez <gilbertomtz672@gmail.com>
2017-10-01 03:24:27 -0500

    Called methods from HVAC

--- Raspberry-Pi/piController/controller/views.py
+++ Raspberry-Pi/piController/controller/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .HVAC import HVAC
+
+unit = HVAC();
 
 def getTemp(request):
-    return HttpResponse("78 degrees cause why not")
+    tuple=unit.get_temperature_humidity()
+    temp=tuple[1]
+    return HttpResponse(str(temp)+" degrees Fahrenheit")
 
 def on(request):
+    unit.turn_ac_on()
     return HttpResponse("AC turned ON")
 
 def off(request):
+    unit.turn_ac_off()
     return HttpResponse("AC turned OFF")
 
 
